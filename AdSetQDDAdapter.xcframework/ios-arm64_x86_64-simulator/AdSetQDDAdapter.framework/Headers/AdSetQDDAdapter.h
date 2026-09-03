5c3f88d03f5b8750ec994c6100205bc8aa66098d
yaohaofei <313888178@qq.com>
2026-09-03 09:19:13 +0800

    feat: release v2.0.0.0

---
+++ AdSetQDDAdapter.xcframework/ios-arm64_x86_64-simulator/AdSetQDDAdapter.framework/Headers/AdSetQDDAdapter.h
@@ -0,0 +1,10 @@
+//
+//  AdSetQDDAdapter.h
+//  AdSetQDDAdapter
+//  AdSetQDD 渠道适配器
+//
+
+#import <Foundation/Foundation.h>
+
+FOUNDATION_EXPORT double AdSetQDDAdapterVersionNumber;
+FOUNDATION_EXPORT const unsigned char AdSetQDDAdapterVersionString[];
